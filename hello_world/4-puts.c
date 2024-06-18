00aa8692b7347194f31ccb557d3e414566a55c74
Elderion10 <yahurifrancisco10@gmail.com>
2024-06-17 21:15:54 -0400

    Hello puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <studio.h>
+
 int main(void)
 {
 
-puts(""Programing in like building a multilingual puzzle\n");
-return 0;
-
+	puts(""Programing is like building a multilingual puzzle\n");
+	return 0;
 
 }
