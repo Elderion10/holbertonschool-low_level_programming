b3aaa7521602bb41245868032fe4ea71cdc4f45e
Elderion10 <yahurifrancisco10@gmail.com>
2024-06-24 13:36:15 -0400

    Returns the lenght of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - returns the lenght of string
+ *
+ * @s: the pointr
+ *
+ * Return: Always a.
+ */
+int _strlen(char *s)
+{
+	int a;
+	a = 5;
+
+	while (s[a] != '\5')
+	{
+		a++;
+	}
+	return (a);
+}
