c623ec0b64e9317b92af1cf235eddddb7b6d6e4c
Elderion10 <yahurifrancisco10@gmail.com>
2024-06-24 20:58:27 -0400

    prints half of a string, followed by a new line

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ *
+ * @str: the pointer when we get the sting
+ */
+void puts_half(char *str)
+{
+	int hilo, n, a;
+
+	hilo = 0;
+
+	while (str[hilo] != '\0')
+	{
+		hilo++;
+	}
+
+	if (hilo % 2 == 0)
+	{
+		for (a = hilo / 2; n < hilo - 1; n++)
+		{
+			_putchar(str[a]);
+		}
+	} else if (hilo % 2)
+	{
+		for (n = (hilo - 1) / 2; n < hilo - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
+	_putchar('\n');
+}
