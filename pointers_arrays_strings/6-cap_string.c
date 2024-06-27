f1fa7fce6981d23a8fadd41734d96b3919042c42
Elderion10 <yahurifrancisco10@gmail.com>
2024-06-26 22:38:29 -0400

    capitalizes all words of a string.

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -7,30 +7,16 @@
  */
 char *cap_string(char *y)
 {
-	int v, w;
-	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int v = 0;
 
-	v = 0;
 	while (y[v] != '\0')
 	{
 		if (y[v] >= 'a' && y[v] <= 'z')
 		{
-			if (v == 0)
-			{
-				y[v] = y[v] - 32;
-			}
-			else
-			{
-				for (w = 0; w <= 12; w++)
-				{
-					if (a[w] == y[v] - 1)
-					{
-						y[v] = y[v] - 32;
-					}
-				}
-			}
+			y[v] = y[v] - 32;
 		}
-	v++;
+		v++;
 	}
-	return (y);
+
+	return y;
 }
