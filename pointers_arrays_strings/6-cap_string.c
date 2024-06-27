969f71a1b686b59132772bf0ea77077c4f54c8eb
Elderion10 <yahurifrancisco10@gmail.com>
2024-06-27 08:41:01 -0400

    capitalizes all words of a string.

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,22 +1,36 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * *cap_string - capitalize all words
- * of a string
- * @y: the pointer to string
- *Return: all words uppercse
+ * cap_string - capitalizes most of the words in a string.
+ * @s: analized string.
+ *
+ * Return: String with all words capitalized.
  */
-char *cap_string(char *y)
+char *cap_string(char *s)
 {
-	int v = 0;
+	int i, j;
+	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	while (y[v] != '\0')
+	i = 0;
+	while (*(s + i) != '\0')
 	{
-		if (y[v] >= 'a' && y[v] <= 'z')
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			y[v] = y[v] - 32;
+			if (i == 0)
+			{
+				*(s + i) = *(s + i) - 32;
+			}
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+					{
+						*(s + i) = *(s + i) - 32;
+					}
+				}
+			}
 		}
-		v++;
+	i++;
 	}
-
-	return y;
+	return (s);
 }
