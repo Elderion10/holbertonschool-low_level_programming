118d47fc356be447f5923959cd18b70f8149fcdd
Elderion10 <yahurifrancisco10@gmail.com>
2024-06-27 20:08:41 -0400

    fills memory with a constant byte

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,12 +2,13 @@
 /**
  * cap_string - capitalizes most of the words in a string.
  * @y: analized string.
+ * @a: stores the separators.
  *
  * Return: String with all words capitalized.
  */
 char *cap_string(char *y)
 {
-	int v, w;
+	int v;
 	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	v = 0;
@@ -20,17 +21,10 @@ char *cap_string(char *y)
 				y[v] = y[v] - 32;
 			}
 			else
-			{
-				for (w = 0; w <= 13; w++)
-				{
-					if (a[w] == y[v] - 1)
-					{
-						y[v] = y[v] - 32;
-					}
-				}
-			}
+
+			for (int w = 0; y[v] == w[a]; v++)
+				return (y);
 		}
-	v++;
+		
 	}
-	return (y);
 }
