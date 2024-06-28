754453fc48fd0f9cf671dca7595ba0c1769cb956
Elderion10 <yahurifrancisco10@gmail.com>
2024-06-27 22:05:42 -0400

    finds the first occurrence of the substring needle in the string haystack. The terminating null bytes (\0) are not compared

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,34 +1,17 @@
 #include "main.h"
 /**
-*_strstr - The _strstr() function finds the first occurrence
-* of the substring needle in the string haystack.
-* The terminating null bytes (\0) are not compared
-*@haystack: string where the search is made
-*@needle: string whose occurence is searched in haystack
-*Return:Returns a pointer to the beginning of the located
-* substring, or NULL if the substring is not found.
-*/
-
-char *_strstr(char *haystack, char *needle)
+ * print_chessboard - print chessboard
+ * @a: 2d array
+ */
+void print_chessboard(char (*a)[8])
 {
-	int i;
+	int row;
+	int column;
 
-	if (*needle == 0)
-		return (haystack);
-
-	while (*haystack)
+	for (row = 0; row < 8; row++)
 	{
-		i = 0;
-
-		if (haystack[i] == needle[i])
-		{
-			do {
-				if (needle[i + 1] == '\0')
-					return (haystack);
-				i++;
-			} while (haystack[i] == needle[i]);
-		}
-		haystack++;
+	for (column = 0; column < 8; column++)
+	_putchar(a[row][column]);
+	_putchar('\n');
 	}
-	return ('\0');
 }
