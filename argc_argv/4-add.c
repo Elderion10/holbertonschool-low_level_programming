23c71850e94441393db2c26dbac5da3a00baac61
Elderion10 <yahurifrancisco10@gmail.com>
2024-07-02 19:16:09 -0400

    adds positive numbers

---
+++ argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers
+ * @argc: counter
+ * @argv: sring
+ * Return: 0 if succes
+ */
+int main(int argc, char **argv)
+{
+	int i, sum;
+
+		sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
