6fe73d4fa3fc17aa08aae724ff6d9dc5ab1e335a
Elderion10 <yahurifrancisco10@gmail.com>
2024-07-02 14:27:36 -0400

    multiplies two numbers

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -16,6 +16,12 @@ int main(int argc, char **argv)
 		return (1);
 	}
 
+	if (argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
 	total = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", total);
 
