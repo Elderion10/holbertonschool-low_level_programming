cc9afef3075e159f0d737e180fe49108488feb40
Elderion10 <yahurifrancisco10@gmail.com>
2024-07-02 13:07:56 -0400

    multiplies two numbers

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - multiplies two numbers
+ * @argc: counts the words
+ * @argv: pointer
+ * Return: 1 if error and 0 if works
+ */
+int main(int argc, char **argv)
+{
+	int total;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	total = (int) *argv[1] * *argv[2];
+
+	printf("%d\n", total);
+
+			return (0);
+}
