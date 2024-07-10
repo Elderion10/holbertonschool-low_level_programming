8c2dbc5d11e9361ddbec47f3a957c198a5e8a589
Elderion10 <yahurifrancisco10@gmail.com>
2024-07-10 11:13:51 -0400

    correccion de file de prototipos

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,3 +1,6 @@
+#ifndef FP
+#define FP
+
 void print_name(char *name, void (*f)(char *));
 
 void array_iterator(int *array, size_t size, void (*action)(int));
@@ -7,3 +10,5 @@ int int_index(int *array, int size, int (*cmp)(int));
 int (*get_op_func(char *s))(int, int);
 
 int _putchar(char c);
+
+#endif
