4dad305652f14d57323ac546190ebd800ebfd464
Elderion10 <yahurifrancisco10@gmail.com>
2024-07-10 18:56:40 -0400

    creates a new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,35 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - function
+ * @name: name of the new dog
+ * @age: age of the new dog
+ * @owner: owner of the dog
+ * Return: p
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+
+	p = malloc(sizeof(dog_t));
+	if (p == NULL)
+		return (NULL);
+
+	if (name == NULL)
+	{
+		free(p);
+		free(owner);
+		return (NULL);
+	}
+	if (owner == NULL)
+	{
+		free(p);
+		free(name);
+		return (NULL);
+	}
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (p);
+}
