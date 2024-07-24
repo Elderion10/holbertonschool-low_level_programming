a86b18bc7ec56292fec77387ee8434777ec63f81
Elderion10 <yahurifrancisco10@gmail.com>
2024-07-24 11:06:21 -0400

    add a second node

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -6,12 +6,19 @@
 void free_list(list_t *head)
 {
 	list_t *nodefree;
+	list_t *node2free;
 
 	nodefree = head;
+	head = NULL;
 
 	nodefree = malloc(sizeof(list_t));
+	node2free = malloc(sizeof(list_t));
 	if (!nodefree)
 	{
 		free(nodefree);
+	}
+	if (!node2free)
+	{
+		free(node2free);
 	}
 }
